432c758f3f3f155f6f22da5c93fe91f43332222c
Alfonso <allacalle@gmail.com>
2025-04-10 13:30:23 +0200

    Ajustes y  cambios de estilos responsive footer y navbar

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import '../css/footer.css'; // Archivo de estilos para el Footer
+import '../css/footer.css';
 import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
+        {/* Sección Contacto */}
         <div className="footer-section">
           <h3>Contacto</h3>
           <p>Dirección: Calle Falsa 123, Córdoba</p>
           <p>Teléfono: +34 123 456 789</p>
           <p>Email: info@latapamix.com</p>
         </div>
+
+        {/* Sección Redes Sociales */}
         <div className="footer-section">
           <h3>Redes Sociales</h3>
           <ul>
@@ -20,16 +23,19 @@ const Footer = () => {
             <li><a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a></li>
           </ul>
         </div>
+
+        {/* Sección Enlaces */}
         <div className="footer-section">
           <h3>Enlaces</h3>
           <ul>
-        <li><Link to="/" >Inicio</Link></li>
-        <li><Link to="/menu" >Menú </Link></li>
-        <li><Link to="/about" >Sobre nosotros</Link></li>
-        <li><a href="/contacto">Contacto</a></li>
-      </ul>
+            <li><Link to="/">Inicio</Link></li>
+            <li><Link to="/menu">Menú</Link></li>
+            <li><Link to="/about">Sobre nosotros</Link></li>
+            <li><Link to="/contacto">Contacto</Link></li>
+          </ul>
         </div>
       </div>
+
       <div className="footer-bottom">
         &copy; {new Date().getFullYear()} La Tapa Mix. Todos los derechos reservados.
       </div>
--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,22 +1,31 @@
-import React from 'react';
-import '../css/navbar.css'; // Archivo de estilos para el Footer
-import logo from '/assets/images/logo.png'
-
-
+import React, { useState } from 'react';
+import '../css/navbar.css';
+import logo from '/assets/images/logo.png';
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'; // Importa los iconos
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="logo-container">
         <div className="logo-text">La Tapa <span>Mix</span></div>
         <img src={logo} alt="Logo" className="logo-image" />
       </div>
-      <ul className="nav-links">
-        <li><Link to="/" >Inicio  </Link></li>
-        <li><Link to="/menu">Menú</Link></li>
-        <li><Link to="/about">Sobre nosotros</Link></li>
-        <li><a href="/contacto">Contacto</a></li>
+      <button className="hamburger-menu" onClick={toggleMenu}>
+        <FontAwesomeIcon icon={isOpen ? faTimes : faBars} />
+      </button>
+      <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
+        <li><Link to="/" onClick={() => setIsOpen(false)}>Inicio  </Link></li>
+        <li><Link to="/menu" onClick={() => setIsOpen(false)}>Menú</Link></li>
+        <li><Link to="/about" onClick={() => setIsOpen(false)}>Sobre nosotros</Link></li>
+        <li><a href="/contacto" onClick={() => setIsOpen(false)}>Contacto</a></li>
       </ul>
     </nav>
   );
